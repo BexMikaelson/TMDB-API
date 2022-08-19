3ac247dc65c4c92e93375101f437c2122b6e1316
Heidi sjöberg <heidi.alexx8@gmail.com>
2022-08-19 14:03:51 +0200

    Starting project

---
+++ tmdb-api-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+import Navigation from './components/Navigation'
+import HomePage from './pages/HomePage'
+import NotFound from './pages/NotFound'
+import './assets/scss/App.scss'
+import './App.css'
+
+function App() {
+	return (
+		<div id="App">
+			<Navigation />
+
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</div>
+	)
+}
+
+export default App

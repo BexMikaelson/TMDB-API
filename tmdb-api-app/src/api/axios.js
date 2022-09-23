1eccebeaaa636d727ae3e24f8998af481ab4e461
Heidi sjöberg <heidi.alexx8@gmail.com>
2022-09-23 14:22:05 +0200

    corecting axios

--- tmdb-api-app/src/api/axios.js
+++ tmdb-api-app/src/api/axios.js
@@ -15,87 +15,32 @@ export const getCharacters= (person_id) => {
     return get(`/person/${person_id}?api_key=${VITE_API_KEY}&language=en-US&include_adult=false&append_to_response=credits`);
 }
 
+export const getHomePage= (pageParam = 1, genreforURL) => {
+    return get(`/discover/movie?api_key=${VITE_API_KEY}&language=en-US&include_adult=false&append_to_response=credits&page=${pageParam}&with_genres=${genreforURL}`)
+}
+
+export const getMovieInfoPage= (movie_id) => {
+    return get(`https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=${VITE_API_KEY}&language=en-US&include_adult=false&append_to_response=credits`)
+}
+
+export const getMostPopularMoviesPage= (pageParam = 1, genreforURL) => {
+    return get(`/movie/popular?api_key=${VITE_API_KEY}&language=en-US&include_adult=false&append_to_response=credits&page=${pageParam}&with_genres=${genreforURL}`)
+}
+
+export const getLatestMoviesPage= (pageParam = 1, genreforURL) => {
+    return get(`/movie/now_playing?api_key=${VITE_API_KEY}&language=en-US&include_adult=false&append_to_response=credits&page=${pageParam}&with_genres=${genreforURL}`)
+}
+
+export const getTopMoviePage= (pageParam = 1, genreforURL) => {
+    return get(`/movie/top_rated?api_key=${VITE_API_KEY}&language=en-US&include_adult=false&append_to_response=credits,&page=${pageParam}&with_genres=${genreforURL}`)
+}
+
+
 export default{
     getCharacters,
+    getHomePage,
+    getMovieInfoPage,
+    getMostPopularMoviesPage,
+    getLatestMoviesPage,
 }
-
-// Get an actor
-// export const getActor = (person_id) => {
-//     return get(`https://api.themoviedb.org/3/person/${person_id}?api_key=05f52796b7985ed1e09a4067b247940c&language=en-US&append_to_response=credits`)
-// }
-
-
-
-
-export const axiosGenreList = axios.create({
-    baseURL: 'https://api.themoviedb.org/3'
-})
-
-export const getGenreListPage = async (pageParam = 1, genreforURL) => {
-    const response = await axiosGenreList.get(`/discover/movie?api_key=05f52796b7985ed1e09a4067b247940c&language=en-US&include_adult=false&append_to_response=credits&page=${pageParam}&with_genres=${genreforURL}`)
-    return response.data
-}
-
-export const axiosMostPopularMovies = axios.create({
-    baseURL: 'https://api.themoviedb.org/3'
-})
-
-export const getMostPopularMoviesPage = async (pageParam = 1, genreforURL) => {
-    const response = await axiosMostPopularMovies.get(`/movie/popular?api_key=05f52796b7985ed1e09a4067b247940c&language=en-US&include_adult=false&append_to_response=credits&page=${pageParam}&with_genres=${genreforURL}`)
-    return response.data
-}
-
-//Get LatestMavies
-export const axiosLatestMovies = axios.create({
-    baseURL: 'https://api.themoviedb.org/3'
-})
-
-export const getLatestMoviesPage = async (pageParam = 1, genreforURL) => {
-    const response = await axiosLatestMovies.get(`/movie/now_playing?api_key=05f52796b7985ed1e09a4067b247940c&language=en-US&include_adult=false&append_to_response=credits&page=${pageParam}&with_genres=${genreforURL}`)
-    return response.data
-}
-
-
-//Get TopMovies
-export const axiosTopMovies = axios.create({
-    baseURL: 'https://api.themoviedb.org/3'
-})
-
-export const getTopMoviePage = async (pageParam = 1, genreforURL) => {
-    const response = await axiosTopMovies.get(`/movie/top_rated?api_key=05f52796b7985ed1e09a4067b247940c&language=en-US&include_adult=false&append_to_response=credits,&page=${pageParam}&with_genres=${genreforURL}`)
-    return response.data
-}
-
-
-// Get an actor
-export const axiosActor = axios.create({
-    baseURL: 'https://api.themoviedb.org/3'
-})
-
-
-export const getActors = async (person_id) => {
-    const response = await axiosActor.get(`/person/${person_id}?api_key=05f52796b7985ed1e09a4067b247940c&language=en-US&append_to_response=credits`)
-    return response.data
-}
-
-//Get the Credits
-export const axiosCredit = axios.create({
-    baseURL: 'https://api.themoviedb.org/3'
-})
-
-
-export const getCredits = async () => {
-    const response = await axiosCredit.get(
-        'https://api.themoviedb.org/3/movie/movie_id/credits?api_key=05f52796b7985ed1e09a4067b247940c&language=en-US'
-      );
-    return response.data
-}
-
-
-
-// Get an actor
-// export const getActor = (person_id) => {
-//     return get(`https://api.themoviedb.org/3/person/${person_id}?api_key=05f52796b7985ed1e09a4067b247940c&language=en-US&append_to_response=credits`)
-// }
-
 

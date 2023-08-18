361ce4b866dc4fec328ffe6f91eccfe4e6549bc0
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-08-18 14:38:50 +0200

    new page for popular movies

---
+++ tmdb-api-app/src/pages/PopularMoviesPage.jsx
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container'
+import { useQuery } from 'react-query'
+import Alert from 'react-bootstrap/Alert'
+import MovieCard from '../components/MovieCard'
+import { getPopularMovies } from '../services/TMDB'
+
+const PopularMoviesPage = () => {
+
+    const { isLoading, isError, error, data} = useQuery(['popularMovies'], getPopularMovies)
+  
+    return (
+      <Container>
+          <h1>PopularMoviesPage</h1>
+              {isLoading && (<p className='my-3'>Loading ....</p>)}
+  
+              {isError && (
+                  <Alert variant="danger">
+                      <h3>error!</h3>
+                      <p>{error.message}</p>
+                  </Alert>)} 
+  
+              {data && <MovieCard data={data} /> }
+  
+      </Container>
+    )
+    }
+  
+  
+  export default PopularMoviesPage

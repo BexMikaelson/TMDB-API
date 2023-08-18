3d3ae5e8fba856c032867b112566db0027660bac
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-08-18 14:36:43 +0200

    adding Cinema movies

---
+++ tmdb-api-app/src/pages/CinemaPage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Alert from 'react-bootstrap/Alert'
+import MovieCard from '../components/MovieCard'
+import { getCinemaMovies } from '../services/TMDB'
+import { useQuery } from 'react-query'
+
+
+
+const CinemaMoviesPage = () => {
+    const {isLoading, isError, error, data} = useQuery(['cinemaMovies'], getCinemaMovies)
+  return (
+    <Container>
+      <h1>Now playing on the big screen</h1>
+          {isLoading && (<p className='my-3'>Loading...</p>)}
+
+          {isError && ( 
+              <Alert variant="danger">
+                <h3>error!</h3>
+                <p>{error.message}</p>
+              </Alert>)}
+      
+        
+          {data && <MovieCard data={data} />}
+        
+      
+    </Container>
+  )
+}
+
+export default CinemaMoviesPage

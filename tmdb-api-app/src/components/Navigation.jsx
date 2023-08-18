1b87b2b7100c6779028e3a2806be732147fcb8c6
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-08-18 13:04:21 +0200

    changing the nav

--- tmdb-api-app/src/components/Navigation.jsx
+++ tmdb-api-app/src/components/Navigation.jsx
@@ -1,36 +1,40 @@
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import { Link, NavLink } from "react-router-dom";
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Dropdown  from 'react-bootstrap/Dropdown'
+import { Link, NavLink } from 'react-router-dom'
+import { getGenres} from '../services/TMDB'
+import { useQuery } from 'react-query'
 
 const Navigation = () => {
+	const {data} = useQuery(['genres'], getGenres)
 	return (
-		<Navbar className="header" bg="dark" variant="dark" expand="md">
+		<Navbar bg="dark" variant="dark" expand="md">
 			<Container>
-				<Navbar.Brand as={Link} to="/">
-					Your Ultimate Movie API
-				</Navbar.Brand>
+				<Navbar.Brand as={Link} to="/">Your Ultimate Movie API</Navbar.Brand>
 
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
-						<Nav.Link as={NavLink} end to="/">
-							Home
-						</Nav.Link>
-						<Nav.Link as={NavLink} end to="/latestMovies">
-							Latest Movies
-						</Nav.Link>
-						<Nav.Link as={NavLink} end to="/topMovies">
-							Top Movies
-						</Nav.Link>
-						<Nav.Link as={NavLink} end to="/mostPopularMovies">
-							Popular Movies
-						</Nav.Link>
+						<Nav.Link as={NavLink} end to="/">Home</Nav.Link>
+						{data && (
+							<Dropdown>
+								<Dropdown.Toggle variant="dark" id="dropdown-basic">Genres</Dropdown.Toggle>
+								<Dropdown.Menu>
+									{data.genres.map(gen => (
+										<Dropdown.Item value="1" key={gen.id} size="sm" as={NavLink} to={`/genres/${gen.id}`}>{gen.name}</Dropdown.Item>
+									))}
+								</Dropdown.Menu>
+							</Dropdown>
+						)}
+						<Nav.Link as={NavLink} end to="/now_playing">In Cinema</Nav.Link>
+						<Nav.Link as={NavLink} end to="/top_rated">Top Rated</Nav.Link>
+						<Nav.Link as={NavLink} end to="/popular">Popular</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
 		</Navbar>
-	);
-};
+	)
+}
 
-export default Navigation;
+export default Navigation
